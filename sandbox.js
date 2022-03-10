2260b98441953a016c8e670455ec778ac4c3204d
Achraf Samoudi <ash-sd@pm.me>
2022-03-11 00:10:13 +0100

    11-Mar_noname

--- sandbox.js
+++ sandbox.js
@@ -6,7 +6,7 @@ let tempRes
 
 // buttons variables
 
-const buttons = document.querySelectorAll('.button')
+const buttons = document.querySelectorAll('.number')
 
 
 buttons.forEach(button => {
@@ -14,20 +14,32 @@ buttons.forEach(button => {
         switch (e.target.innerText) {
             case 'C':
                 output.innerText = '0'
+                temp.innerText = '0'
                 break;
             case '←':
                 if(output.innerText > '0'){
                     output.innerText = output.innerText.slice(0,-1)
+                    if(!output.innerText){
+                        output.innerText= '0'
+                    }
                 }
                 break;
             case '+':
-                temp.innerText += output.innerText
+                if (!output.innerText) {
+                    temp.innerText = output.innerText
+                }
+                else{
+                    temp.innerText += output.innerText
+                }
                 break;
             default:
                 if (output.innerText === '0') {
                     output.innerText = e.target.innerText
                 }
                 else{
+                    if (e.target.textContent === '.' && output.innerText.includes('.')) {
+                        return
+                    }
                     output.innerText += e.target.innerText
                 }
                 break;
