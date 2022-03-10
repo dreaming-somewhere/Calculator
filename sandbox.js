5217e4af45d1cd7b5187c033d16b35844d19e14d
Achraf Samoudi <ash-sd@pm.me>
2022-03-10 23:15:17 +0100

    10-Mar_noname

--- sandbox.js
+++ sandbox.js
@@ -11,11 +11,26 @@ const buttons = document.querySelectorAll('.button')
 
 buttons.forEach(button => {
     button.addEventListener('click', e => {
-        if (output.innerText === '0') {
-            output.innerText = e.target.innerText
-        }
-        else{
-            output.innerText += e.target.innerText
+        switch (e.target.innerText) {
+            case 'C':
+                output.innerText = '0'
+                break;
+            case '←':
+                if(output.innerText > '0'){
+                    output.innerText = output.innerText.slice(0,-1)
+                }
+                break;
+            case '+':
+                temp.innerText += output.innerText
+                break;
+            default:
+                if (output.innerText === '0') {
+                    output.innerText = e.target.innerText
+                }
+                else{
+                    output.innerText += e.target.innerText
+                }
+                break;
         }
     })
 });
